c9badbb90cc9adbc0d43c4b484e6a79739dc283c
IganoSaigo <an.grigoriev84@gmail.com>
2024-04-20 17:01:17 +0300

    post schema change inheritance

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -13,49 +13,6 @@ def gen_post_date() -> str:
     return datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
 
 
-# ----> DB's schemas
-
-
-class PostFromDB(BaseModel):
-    id: int
-    post_id: str
-    writer: str
-    writer_id: int
-    title: str
-    description: Optional[str] = None
-    content: Optional[str] = None
-    tags: List[Optional[str]]
-    created: datetime
-    modified: Union[None, datetime]
-    estimated: int
-
-
-class PostToDB(BaseModel):
-    title: str
-    description: str
-    content: str
-    tags: List[Optional[str]]
-    estimated: int
-
-
-class CreatePostInDB(PostToDB):
-    post_id: str | None = Field(default=None, validate_default=True)
-    created: str = Field(default_factory=gen_post_date)
-    modified: None = None
-
-    @field_validator("post_id", mode="after")
-    @classmethod
-    def make_post_id(cls, v: str | None, info: ValidationInfo) -> str:
-        if v:
-            return v
-        title = info.data["title"]
-        return gen_post_id(title)
-
-
-class UpdatePostInDB(PostToDB):
-    modified: str = Field(default_factory=gen_post_date)
-
-
 # ----> HTTP Responses schemas
 
 
@@ -63,9 +20,16 @@ class TagsResponse(BaseModel):
     tags: List[str]
 
 
-class PostResponse(PostFromDB):
-    writer_id: int = Field(exclude=True)
-    id: int = Field(exclude=True)
+class PostResponse(BaseModel):
+    post_id: str
+    writer: str
+    title: str
+    description: Optional[str] = None
+    content: Optional[str] = None
+    tags: List[Optional[str]]
+    created: datetime
+    modified: Union[None, datetime]
+    estimated: int
 
 
 class PageResponse(BaseModel):
@@ -98,3 +62,37 @@ class CreatePostRequest(BaseModel):
 
 class UpdatePostRequest(CreatePostRequest):
     pass
+
+
+# ----> DB's schemas
+
+
+class PostFromDB(PostResponse):
+    id: int
+    writer_id: int
+
+
+class PostToDB(BaseModel):
+    title: str
+    description: str
+    content: str
+    tags: List[Optional[str]]
+    estimated: int
+
+
+class CreatePostInDB(PostToDB):
+    post_id: str | None = Field(default=None, validate_default=True)
+    created: str = Field(default_factory=gen_post_date)
+    modified: None = None
+
+    @field_validator("post_id", mode="after")
+    @classmethod
+    def make_post_id(cls, v: str | None, info: ValidationInfo) -> str:
+        if v:
+            return v
+        title = info.data["title"]
+        return gen_post_id(title)
+
+
+class UpdatePostInDB(PostToDB):
+    modified: str = Field(default_factory=gen_post_date)
